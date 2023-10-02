f6fa66fce2788b54e0e220a832cd9bc1bccb7157
BC2711 <binesschama1127@gmail.com>
2023-10-02 10:51:00 +0200

    updates

---
+++ eloan/app/Http/Controllers/ClubsSocietiesTrustsController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ClubsSocietiesTrustsController extends Controller
+{
+    public function index()
+    {
+     //
+    }
+ 
+    public function create()
+    {
+     return redirect()->route('');
+    }
+ 
+    public function store(request $request)
+    {
+ 
+    }
+ 
+    public function edit()
+    {
+     return redirect()->route('');
+    }
+ 
+    public function update(Request $request)
+    {
+ 
+    }
+ 
+    public function destroy()
+    {
+     
+    }
+}
